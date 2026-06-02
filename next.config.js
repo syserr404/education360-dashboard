799983f77d89e5b8bc91415cf1f2aa1f4ad5e3a6
Tishy <syserr404@gmail.com>
2026-06-02 15:04:34 +0530

    Change for 308 error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false,
+  skipTrailingSlashRedirect: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin',  value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Accept, Authorization' },
+          { key: 'Access-Control-Max-Age',        value: '86400' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
